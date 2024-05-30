dedd4fc53a95d36206cc47353d1faadbba6e6491
Phuong Nguyen <phuongnguyen@Phuongs-MacBook-Pro-2.local>
2024-05-30 16:42:17 +0700

    Task: Change site config, #norefs

--- site.config.js
+++ site.config.js
@@ -1,30 +1,30 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Phuong Nguyen",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Software Engineer",
+    bio: "I develop everything with passion and love.",
+    email: "phuong2612.mobile@gmail.com",
+    linkedin: "nguyễn-thanh-phương-4a3b20242",
+    github: "wolf-tp",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `tech-log`,
+      href: "https://github.com/wolf-tp/tech-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "tech-log",
+    description: "welcome to tech-log!",
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://tech-log-alpha.vercel.app/",
+  since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
